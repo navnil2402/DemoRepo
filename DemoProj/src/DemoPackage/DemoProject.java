70b4547808c0289b9a8aa9e377c6041965440e7d
navnilniraj <navnil.niraj@flo-group.com>
2024-08-12 22:32:22 +0530

    first commit

---
+++ DemoProj/src/DemoPackage/DemoProject.java
@@ -0,0 +1,10 @@
+package DemoPackage;
+
+public class DemoProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Navnil Niraj");
+	}
+
+}
